00f0e43cacf7bf0b281b09d072ce0651e5da4f54
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-10-08 17:09:39 -0600

    Construct movieCard

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -2,7 +2,14 @@ import React from 'react';
 import './MovieCard.css';
 
 const MovieCard = () => {
-
+    return (
+        <section>
+            <h3> Movie Title </h3>
+            <h3>Episode: <span>Episode</span></h3>
+            <h3>Release Year: <span>1234</span></h3>
+            <button> Characters </button>
+        </section>
+    )
 }
 
 export default MovieCard;
