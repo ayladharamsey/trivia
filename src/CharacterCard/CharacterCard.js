efac4ad11310a2db4d3c3cafbeb70ef794b6e162
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-10-08 17:15:09 -0600

    Construct CharacterCard

--- src/CharacterCard/CharacterCard.js
+++ src/CharacterCard/CharacterCard.js
@@ -1,8 +1,17 @@
-import react from 'react';
+import React from 'react';
 import './CharacterCard.css';
 
 const CharacterCard = () => {
-
+    return (
+        <article>
+            <h3> Name: <span>Name</span></h3>
+            <h3> Homeworld: <span>Homeworld</span></h3>
+            <h3> Population: <span>Population</span></h3>
+            <h3> Species: <span>Species</span></h3>
+            <h3> Other Films: <span> Films</span></h3>
+            <img></img>
+        </article>
+    )
 }
 
 export default CharacterCard;
