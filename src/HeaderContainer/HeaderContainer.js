c99a16d9281feac8b8d12bbbb9299973a5af731f
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-10-08 17:11:27 -0600

    Contruct HeaderContainer

--- src/HeaderContainer/HeaderContainer.js
+++ src/HeaderContainer/HeaderContainer.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './HeaderContainer.css';
+import Nav from '../Nav/Nav';
+import UserProfile from '../UserProfile/UserProfile'
 
 const HeaderContainer = () => {
-
+    return (
+        <section>
+            <Nav />
+            <UserProfile />
+        </section>
+    )
 }
 
 export default HeaderContainer;
