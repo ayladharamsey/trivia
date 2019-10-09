36639972d5b80398595f052a57d740228b832df5
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-10-09 16:54:51 -0600

    Add PropTypes

--- src/CharacterCard/CharacterCard.js
+++ src/CharacterCard/CharacterCard.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 import './CharacterCard.css';
 
-const CharacterCard = () => {
-    return (
-        <article>
-            <h3> Name: <span>Name</span></h3>
-            <h3> Homeworld: <span>Homeworld</span></h3>
-            <h3> Population: <span>Population</span></h3>
-            <h3> Species: <span>Species</span></h3>
-            <h3> Other Films: <span> Films</span></h3>
-            <img alt='fave'></img>
-        </article>
-    )
+class CharacterCard extends Component {
+    constructor() {
+        super();
+        this.state ={
+            isFavorited: false
+        }
+    }
+    
+
+    render() {
+        return (
+            <article>
+                <h3> Name: <span>Name</span></h3>
+                <h3> Homeworld: <span>Homeworld</span></h3>
+                <h3> Population: <span>Population</span></h3>
+                <h3> Species: <span>Species</span></h3>
+                <h3> Other Films: <span> Films</span></h3>
+                <img alt='fave'></img>
+            </article>
+        )
+
+    }
+    
 }
 
 export default CharacterCard;
+
+CharacterCard.propTypes = {
+    isFavorited: PropTypes.bool
+  }
--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './Form.css';
 import logo from '../images/logo.png'
 
@@ -64,3 +65,9 @@ class Form extends Component {
 }
 
 export default Form;
+
+Form.propTypes = {
+    name: PropTypes.string,
+    quote: PropTypes.string,
+    rating: PropTypes.string
+}
