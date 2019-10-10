90944cf693234c7f3ff737fb2f4238544d8bca08
Jon O'Drobinak <jodrobinak@gmail.com>
2019-10-10 09:09:23 -0600

    Modify form of data returned in movie data

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -7,14 +7,12 @@ export const movieTitles = moviesUrl => {
       const { title, characters } = movie;
       const characterData = characters.map(character => {
         return getCharacters(character)
-      .then(name => ({ title, name }))
+      .then(name => ({ name }))
       // .then(something => console.log('something is ', something))
       });
       return Promise.all(characterData)
     });
-    return Promise.all(movieData);
-  })
-  // .then(cbMovies => console.log('could be movies: ', cbMovies))
+    return Promise.all(movieData);})
   .catch(error => console.log(error.message, 'Holy bat smoke batman, something went wrong!'))
 }
 
@@ -23,7 +21,7 @@ const getCharacters = charactersUrl => {
   return fetch (proxyurl + charactersUrl)
   .then(response => response.json())
   .then(data => {
-    console.log('character data name is: ', data.name);
+    // console.log('character data name is: ', data.name);
     return data.name})
   .catch(error => console.log(error.message, 'Holy bat smoke batman, something went wrong with our characters!'))
 }
