911fd3f2de4c550d6472b37f2671219c88930267
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-10-14 18:46:30 -0600

    MovieCard snapshot added

--- src/MovieCard/MovieCard.test.js
+++ src/MovieCard/MovieCard.test.js
@@ -3,13 +3,32 @@ import { shallow } from 'enzyme';
 import MovieCard from './MovieCard';
 
 describe('MovieCard', () => {
-
+  let wrapper, object ;
   beforeEach(() => {
+    
+    object = {
+      characters: ['url.com', 'url.com', 'url.com'],
+      episode_id: 1,
+      opening_crawl: 'scrawl the page with an intro',
+      planets: ['url.com', 'url.com', 'url.com'],
+      release_date: "2002-05-16",
+      species: ['url.com', 'url.com', 'url.com'],
+      title: 'starwars'
+    }
 
+    wrapper = shallow(<MovieCard 
+      characters={object.characters}
+      episode_id={object.episode_id}
+      opening_crawl={object.opening_crawl}
+      planets={object.planets}
+      release_date={object.release_date}
+      species={object.species}
+      title={object.title}
+    />)
   });
 
-  it('should do something cool', () => {
-
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
   });
 
 
