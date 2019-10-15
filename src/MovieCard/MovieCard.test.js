ef002d320fb6fe0362e356cca3e137b2d921993c
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-10-14 18:55:30 -0600

    Nav snapshot created

--- src/MovieCard/MovieCard.test.js
+++ src/MovieCard/MovieCard.test.js
@@ -7,23 +7,19 @@ describe('MovieCard', () => {
   beforeEach(() => {
     
     object = {
+      id: 1,
+      title: 'starwars',
+      episode: 1,
+      release_year: "2002",
       characters: ['url.com', 'url.com', 'url.com'],
-      episode_id: 1,
-      opening_crawl: 'scrawl the page with an intro',
-      planets: ['url.com', 'url.com', 'url.com'],
-      release_date: "2002-05-16",
-      species: ['url.com', 'url.com', 'url.com'],
-      title: 'starwars'
     }
 
     wrapper = shallow(<MovieCard 
-      characters={object.characters}
+      id={object.id}
       episode_id={object.episode_id}
-      opening_crawl={object.opening_crawl}
-      planets={object.planets}
-      release_date={object.release_date}
-      species={object.species}
+      release_year={object.release_year}
       title={object.title}
+      characters={object.characters}
     />)
   });
 
