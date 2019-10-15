dbe1d12f8b18023e184d11fc31566062d2e9fe22
Jon O'Drobinak <jodrobinak@gmail.com>
2019-10-14 22:28:30 -0600

    Update caharacter card, character data

--- src/CharacterCard/CharacterCards.js
+++ src/CharacterCard/CharacterCards.js
@@ -2,8 +2,10 @@ import React from 'react';
 import './CharacterCard.css';
 import Star from "../assets/star.svg" 
 
-const CharacterCards = ({ id, name, homeworld, species, favoriteCards }) => {
+const CharacterCards = ({ id, name, homeworld, species, films, favoriteCards }) => {
+  console.log('CharacterCards is here', name)
   return (
+    
     <section className="card">
         <article className="card-title">
         <h3> Name: {name}</h3>
@@ -11,6 +13,7 @@ const CharacterCards = ({ id, name, homeworld, species, favoriteCards }) => {
         <article className="card-contents">
         <h3>Homeworld: {homeworld}</h3>
         <h3>Species: {species}</h3>
+        <h3>Films: {films}</h3>
         </article>
     </section>
   )
