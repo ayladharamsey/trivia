7e77394b5d38e3547b6abf187624394c5d3793bb
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-10-14 18:37:33 -0600

    Header Container snapshot created

--- src/HeaderContainer/HeaderContainer.test.js
+++ src/HeaderContainer/HeaderContainer.test.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import HeaderContainer from './HeaderContainer';
+import { isJSXEmptyExpression } from '@babel/types';
 
 describe('HeaderContainer', () => {
+  let wrapper;
 
   beforeEach(() => {
-
+    wrapper = shallow(<HeaderContainer 
+      name = 'Ayla'
+      quote =  'she is an an average human'
+      rating = 'master'
+      signOut = {jest.fn()}
+    />)
   });
 
-  it('should do something cool', () => {
-
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
   });
 
 
